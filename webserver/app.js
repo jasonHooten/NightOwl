c9858f47e76e8519e33a46eb4224b043fde3dba4
Jason Hooten <jason.hooten@gmail.com>
2014-05-26 17:26:22 -0500

    fixed the dynamic port heroku

--- webserver/app.js
+++ webserver/app.js
@@ -46,8 +46,8 @@ app.dynamicHelpers(helpers);
 //-----------------------------------------
 // Start server
 //-----------------------------------------
-var port = parseInt(process.argv[2], 10) || 3000;
-app.listen(port);
+
+app.listen(.listen(process.env.PORT || 3000));
 if (app.address())
   console.log("NightOwl server listening on port %d in %s mode", app.address().port, app.settings.env);
 else
