6f00442344fd6d4aa619fbe2bc2b41ec07c8df2d
Jason Hooten <jason.hooten@gmail.com>
2014-05-26 17:36:38 -0500

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node server.js; node webserver/app.js
--- webserver/app.js
+++ webserver/app.js
@@ -46,8 +46,11 @@ app.dynamicHelpers(helpers);
 //-----------------------------------------
 // Start server
 //-----------------------------------------
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
-app.listen(.listen(process.env.PORT || 3000));
 if (app.address())
   console.log("NightOwl server listening on port %d in %s mode", app.address().port, app.settings.env);
 else
